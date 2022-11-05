eaa922c0e0393a881a107da60267f87db15e0eda
EC2 Default User <ec2-user@ip-172-31-37-29.ap-south-1.compute.internal>
2022-11-05 01:39:23 +0000

    commit 6

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,26 @@
 echo "DEPLOY STEP IS RUNNING"
-docker-compose up -d
-docker ps
+echo "What is your Name:"
+
+read name
+
+echo "Enter your dream home name"
+
+read home
+
+
+sleep 2
+
+echo "Calculating"
+echo "    ."
+sleep 1
+echo "  .   ."
+sleep 1
+echo " . . . ."
+sleep 1
+echo " .     ."
+sleep 1
+echo " .     ."
+sleep 1
+echo " . . . ."
+
+echo " $name , Your Dream Home $home"docker-compose up -d
