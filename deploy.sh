c59feb2047dbc464c89569ab955655b2afd32258
EC2 Default User <ec2-user@ip-172-31-37-29.ap-south-1.compute.internal>
2022-11-05 03:31:00 +0000

    commit 7

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,5 @@ echo "What is your Name:"
 
 read name
 
-echo "Enter your dream home name"
+echo "Hi $name"
 
-read home
-
-
-sleep 2
-
-echo "Calculating"
-echo "    ."
-sleep 1
-echo "  .   ."
-sleep 1
-echo " . . . ."
-sleep 1
-echo " .     ."
-sleep 1
-echo " .     ."
-sleep 1
-echo " . . . ."
-
-echo " $name , Your Dream Home $home"docker-compose up -d
